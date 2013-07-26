24fcbf74bf42dab1b05395c17a7abeb9a932d810
Mitch Seaman <mitchel.seaman@gmail.com>
2013-07-25 21:35:42 -0700

    remove extra table migrations

---
+++ db/migrate/20130724120647_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email, unique: true
+      t.string :password_hash
+      t.string :first_name
+      t.string :last_name
+      t.timestamps
+    end
+  end
+end
