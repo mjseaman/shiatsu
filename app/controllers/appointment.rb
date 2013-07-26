9a5845e3b6d7e6fad2365ae173129364e17ae93d
Apprentice <apprentice@dbc09.local>
2013-07-26 16:44:22 -0700

    functional v1 of creating appointment with bootstrap control

--- app/controllers/appointment.rb
+++ app/controllers/appointment.rb
@@ -1,4 +1,3 @@
-
 get '/appointments' do
   erb :appointments_book if 
   redirect '/'
@@ -10,3 +9,39 @@ put '/appointments/book' do
   appointment.save
   current_user.appt_count += 1
 end
+
+# get '/appointments' do
+
+# end
+
+get '/appointments/new' do
+  erb :new_appointment
+end
+
+post '/appointments' do
+  "success"
+end
+
+# get '/appointments/:id' do
+# end
+
+# get '/appointments/:id/edit' do
+# end
+
+# put '/appointments/:id' do
+# end
+
+# delete '/appointments/:id' do
+# end
+
+
+
+
+# GET /photos index display a list of all photos
+# GET /photos/new new return an HTML form for creating a new photo
+# POST  /photos create  create a new photo
+# GET /photos/:id show  display a specific photo
+# GET /photos/:id/edit  edit  return an HTML form for editing a photo
+# PATCH/PUT /photos/:id update  update a specific photo
+# DELETE  /photos/:id destroy delete a specific photo
+
--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,15 +6,3 @@ not_found do
 	@request = request.path_info
 	erb :not_found, layout: false
 end
-
-
-
-
-
-# GET /photos index display a list of all photos
-# GET /photos/new new return an HTML form for creating a new photo
-# POST  /photos create  create a new photo
-# GET /photos/:id show  display a specific photo
-# GET /photos/:id/edit  edit  return an HTML form for editing a photo
-# PATCH/PUT /photos/:id update  update a specific photo
-# DELETE  /photos/:id destroy delete a specific photo
